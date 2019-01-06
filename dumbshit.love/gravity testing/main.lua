acf9cbaca5f3a0d9f3697174b33b9af3ea67f16a
Jaafar Kaoussarani <jaafar.kaoussarani@polymtl.ca>
2019-01-06 16:02:02 -0500

    better jumping

--- dumbshit.love/gravity testing/main.lua
+++ dumbshit.love/gravity testing/main.lua
@@ -45,7 +45,6 @@ function love.update(dt)
 	if player.y_velocity ~= 0 then
     if not love.keyboard.isDown('space') and not player.falling then
       player.falling = true
-      player.y_velocity = 0
     end
 		player.y = player.y + player.y_velocity * dt
 		player.y_velocity = player.y_velocity - player.gravity * dt
