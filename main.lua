8b70419f4a763c9581be2999e7411bbf8b853a7a
Weiliang Xie <weiliangxie@hotmail.com>
2019-01-26 10:56:09 -0500

    Added main functions

--- main.lua
+++ main.lua
@@ -1,3 +1,16 @@
---  Authors: William Xie, Jaafar Kaoussarani  --
+--  Authors: William Xie, Jaafar Kaoussarani, Marianick Benoit, Jason Ma  --
 --  File: main.lua                            --
 --  Description: Executes the program         --
+
+function love.load()
+  
+end
+
+function love.update()
+  
+end
+
+
+function love.draw()
+  
+end
