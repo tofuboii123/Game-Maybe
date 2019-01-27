28eef17f50692f323f1e317211a8d22173d21d5d
Jaafar <jafk1998@gmail.com>
2019-01-26 21:11:10 -0500

    menu good

--- THE GAME ITSELF/menu.lua
+++ THE GAME ITSELF/menu.lua
@@ -6,11 +6,14 @@ menuTimer = 5
 
 function Menu:new()
   self.menuState = 0
+  self.music = love.audio.newSource("Music/Arabic 1.wav","static")
   self.logo = WOW
 end
 
 function Menu:update(dt)
+  love.audio.play(self.music)
   if love.keyboard.isDown("return") and self.menuState == 0 and menuTimer == 5 then
+    love.audio.stop(self.music)
     menu = false
   end
   if love.keyboard.isDown("down") and menuTimer == 5 then
@@ -31,6 +34,11 @@ function Menu:update(dt)
 end
 
 function Menu:draw()
+  love.graphics.setColor(1,1,1)
+  love.graphics.setBackgroundColor(0.41, 0.53, 0.97)
+  love.graphics.circle("fill", 1280/2 , 200, 100)
+  love.graphics.setColor(0.52, 0.46, 0.39)
+  love.graphics.polygon("fill", 0, 720, 320, 650, 640, 720, 0+960, 650, 320 + 960, 650, 640 +960, 720)
   if self.menuState == 0 then
     love.graphics.setColor(0,1,0)
     love.graphics.rectangle("fill", (1280/2 - 150/2), (720/2 - 50/2), 150, 50)
